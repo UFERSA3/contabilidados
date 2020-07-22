6080ac5a6e7c0f67aa17ae80b6355404da6dbcc8
kleberformiga <mirandakf@ufersa.edu.br>
2020-07-22 10:49:56 -0300

    inclusao da funcao corstars que coloca a correlacao com a significancia

--- R/contabilidados.R
+++ R/contabilidados.R
@@ -296,3 +296,62 @@ meses <- data.frame(mes.num = 1:12,
   
 rm(mes.nome)
 
+
+
+
+################ Instruções #####################################################
+# Objetivo: Gerar uma tabela de correlacao com identificação da significância
+# estatística.
+# 
+# Adaptado de:
+# http://www.sthda.com/english/wiki/elegant-correlation-table-using-xtable-r-package
+#################################################################################
+
+corstars <- function(x, method=c("pearson", "spearman"),
+                     removeTriangle=c("upper", "lower"),
+                     result=c("none", "html", "latex"),
+                     digits = 4){
+  #Compute correlation matrix
+  require(Hmisc)
+  x <- as.matrix(x)
+  correlation_matrix<-rcorr(x, type=method[1])
+  R <- correlation_matrix$r # Matrix of correlation coeficients
+  p <- correlation_matrix$P # Matrix of p-value 
+  
+  ## Define notions for significance levels; spacing is important.
+  mystars <- ifelse(p < .01, "***",
+                    ifelse(p < .05, "** ",
+                           ifelse(p < .1, "*   ", "    ")))
+  
+  ## trunctuate the correlation matrix to four decimal (default)
+  R <- format(round(cbind(rep(-1.11, ncol(x)), R), digits))[,-1]
+  
+  ## build a new matrix that includes the correlations with their apropriate stars
+  Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x))
+  diag(Rnew) <- paste(diag(R), " ", sep="")
+  rownames(Rnew) <- colnames(x)
+  colnames(Rnew) <- paste(colnames(x), "", sep="")
+  
+  ## remove upper triangle of correlation matrix
+  if(removeTriangle[1]=="upper"){
+    Rnew <- as.matrix(Rnew)
+    Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
+    Rnew <- as.data.frame(Rnew)
+  }
+  
+  ## remove lower triangle of correlation matrix
+  else if(removeTriangle[1]=="lower"){
+    Rnew <- as.matrix(Rnew)
+    Rnew[lower.tri(Rnew, diag = TRUE)] <- ""
+    Rnew <- as.data.frame(Rnew)
+  }
+  
+  ## remove last column and return the correlation matrix
+  Rnew <- cbind(Rnew[1:length(Rnew)-1])
+  if (result[1]=="none") return(Rnew)
+  else{
+    if(result[1]=="html") print(xtable(Rnew), type="html")
+    else print(xtable(Rnew), type="latex") 
+  }
+} 
+
